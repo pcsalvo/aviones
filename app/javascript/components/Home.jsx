ec74048ebce187b7de836cc596c6d73a40b15b81
pcsalvo <pcsalvo@uc.cl>
2021-05-23 20:30:54 -0400

    mapa

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { Link } from "react-router-dom";
 
+const tilesProvider = "https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png"
+
+let myMap = L.map('myMap').setView([51.505, -0.09], 13)
+
+
 export default () => (
-  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
-    <div className="jumbotron jumbotron-fluid bg-transparent">
-      <div className="container secondary-color">
-        <h1 className="display-4">Avioncitos</h1>
-        <p className="lead">
-          Podrás encontrar el estado del vuelo de los avioncitos
-        </p>
-      </div>
+  <div>
+    <h1 className="display-4">Avioncitos</h1>
+    <p className="lead">
+        Podrás encontrar el estado del vuelo de los avioncitos
+    </p>
+    <div id="myMap" style="height: 600px">
+        <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+        integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+        crossorigin=""></script>
     </div>
   </div>
 );
